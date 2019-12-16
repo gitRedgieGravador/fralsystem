f3422aa1cb9a4ea2882b10342229a28d0659028a
gitRedgieGravador <iderge32@gmail.com>
2019-12-16 14:15:24 +0800

    initial commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+
+      <v-spacer></v-spacer>
+
+      <v-btn text>
+        <v-icon>mdi-login</v-icon>
+        <span class="mr-2">Log in</span>
+        
+      </v-btn>
+      <v-btn text>
+        <span class="mr-2">Log out</span>
+        <v-icon>mdi-logout</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <!-- <v-content>
+      <HelloWorld/>
+    </v-content> -->
+  </v-app>
+</template>
+
+<script>
+//import HelloWorld from './components/HelloWorld';
+
+export default {
+  name: 'App',
+
+  components: {
+    //HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ frontend/src/router/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import Admin from '../views/admin'
+
+Vue.use(VueRouter)
+
+const routes = [{
+    path: '/',
+    name: 'admin',
+    component: Admin
+}, ]
+
+const router = new VueRouter({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
+})
+
+export default router
